e2171c660d86dae2ef10d6667093a6fa994c8b70
[lxin42140] <[lxin42140@gmail.com]>
2020-05-22 22:59:56 +0800

    removed store from layout

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,47 +1,31 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
+//import { connect } from "react-redux";
 
 import classes from "./Layout.css";
-import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
-import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 
 class Layout extends Component {
-  state = {
-    showSideDrawer: false,
-  };
 
-  sideDrawerClosedHandler = () => {
-    this.setState({ showSideDrawer: false });
-  };
+  // state = {
+  //   showSideDrawer: false,
+  // };
 
-  sideDrawerToggleHandler = () => {
-    this.setState((prevState) => {
-      return { showSideDrawer: !prevState.showSideDrawer };
-    });
-  };
+  // sideDrawerClosedHandler = () => {
+  //   this.setState({ showSideDrawer: false });
+  // };
+
+  // sideDrawerToggleHandler = () => {
+  //   this.setState((prevState) => {
+  //     return { showSideDrawer: !prevState.showSideDrawer };
+  //   });
+  // };
 
   render() {
     return (
       <React.Fragment>
-        <Toolbar
-          isAuthenticated={this.props.isAuthenticated}
-          drawerToggleClicked={this.sideDrawerToggleHandler}
-        />
-        <SideDrawer
-          isAuthenticated={this.props.isAuthenticated}
-          open={this.state.showSideDrawer}
-          closed={this.sideDrawerClosedHandler}
-        />
         <main className={classes.Content}>{this.props.children}</main>
       </React.Fragment>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    isAuthenticated: state.auth.token !== null,
-  };
-};
-
-export default connect(mapStateToProps)(Layout);
+export default Layout;
