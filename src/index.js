319f96f7b59f462a2836be2479b3bf0e0abc66b4
[lxin42140] <[lxin42140@gmail.com]>
2020-05-22 22:58:37 +0800

    added redux store

--- src/index.js
+++ src/index.js
@@ -1,34 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
-import React from "react";
-import ReactDOM from "react-dom";
 
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import burgerReducer from "./store/reducers/burgerBuilder";
-import orderReducer from "./store/reducers/order";
-import authReducer from "./store/reducers/auth";
+import searchBarReducer from './store/reducers/searchBar';
 
-// const rootReducer = combineReducers({
-//   burgerBuilder: burgerReducer,
-//   order: orderReducer,
-//   auth: authReducer
-// });
+const rootReducer = combineReducers({
+  search: searchBarReducer,
+});
 
-const composeEnhancer = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose : null;
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
-// const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
-  //<Provider store={store}>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  //</Provider>
+  </Provider>
 );
 
 ReactDOM.render(app, document.getElementById("root"));
 registerServiceWorker();
+
+
