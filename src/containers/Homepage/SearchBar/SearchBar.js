2e5cc041e99ae1d4dc67bc19dbda79164ae0f99d
[lxin42140] <[lxin42140@gmail.com]>
2020-05-23 17:18:39 +0800

    added dispatch

--- src/containers/Homepage/SearchBar/SearchBar.js
+++ src/containers/Homepage/SearchBar/SearchBar.js
@@ -7,10 +7,18 @@ import withErrorHandler from "../../../hoc/withErrorHandler/withErrorHandler";
 import classes from "./SearchBar.css";
 import * as actions from "../../../store/actions/index";
 import Button from "../../../components/UI/Button/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFilter,
+  faLocationArrow,
+  faBook,
+  faUniversity,
+} from "@fortawesome/free-solid-svg-icons";
 
 class SearchBar extends Component {
   state = {
     filterType: "module code",
+    showDropDown: false,
   };
 
   componentDidMount() {
@@ -21,47 +29,76 @@ class SearchBar extends Component {
     switch (filter) {
       case "locations":
         this.setState({
+          showDropDown: false,
           filterType: "preferred location",
         });
         break;
       case "titles":
         this.setState({
+          showDropDown: false,
           filterType: "book title",
         });
         break;
       default:
         this.setState({
+          showDropDown: false,
           filterType: "module code",
         });
         break;
     }
   };
 
+  filterDropdownHandler = (event) => {
+    this.setState((prevState) => ({
+      showDropDown: !prevState.showDropDown,
+    }));
+  };
+
   render() {
+    let dropDown = null;
+
+    if (this.state.showDropDown) {
+      dropDown = (
+        <React.Fragment>
+          <span className={classes.dropDownElement}>
+            <FontAwesomeIcon
+              icon={faLocationArrow}
+              style={{ padding: "0 3px", fontSize: "1rem", color: "#ff9f90" }}
+            />
+            <a onClick={() => this.changeFilterHandler("locations")}>
+              location
+            </a>
+          </span>
+          <span className={classes.dropDownElement}>
+            <FontAwesomeIcon
+              icon={faBook}
+              style={{ padding: "0 3px", fontSize: "1rem", color: "#ff9f90" }}
+            />
+            <a onClick={() => this.changeFilterHandler("titles")}>book title</a>
+          </span>
+          <span className={classes.dropDownElement}>
+            <FontAwesomeIcon
+              icon={faUniversity}
+              style={{ padding: "0 3px", fontSize: "1rem", color: "#ff9f90" }}
+            />
+            <a onClick={() => this.changeFilterHandler("modules")}>
+              module code
+            </a>
+          </span>
+        </React.Fragment>
+      );
+    }
+
     return (
-      <div className={classes.SearchBar}>
+      <div className={classes.searchbar}>
+        <div className={classes.dropDown}>
+          <Button onClick={this.filterDropdownHandler}>Filter by</Button>
+          <div className={classes.dropdownContent}>{dropDown}</div>
+        </div>
         <AutoComplete
-          filterType={this.state.filterType}
           suggestions={this.props.modules}
+          filterType={this.state.filterType}
         />
-        <Button
-          btnType="Success"
-          onClick={() => this.changeFilterHandler("locations")}
-        >
-          by location
-        </Button>
-        <Button
-          btnType="Success"
-          onClick={() => this.changeFilterHandler("titles")}
-        >
-          by book title
-        </Button>
-        <Button
-          btnType="Success"
-          onClick={() => this.changeFilterHandler("modules")}
-        >
-          by module code
-        </Button>
       </div>
     );
   }
