ada5f5bdc9cabfff445b66e0fa968bab43312d58
[lxin42140] <[lxin42140@gmail.com]>
2020-05-24 15:51:20 +0800

    changed reducer

--- src/App.js
+++ src/App.js
@@ -3,42 +3,20 @@ import { Route, withRouter, Switch, Redirect } from "react-router-dom";
 
 import Layout from "./hoc/Layout/Layout";
 
-// import Logout from "./containers/Auth/Logout/Logout";
-// import { connect } from "react-redux";
-// import * as actions from "../src/store/actions/index";
 import asyncComponent from "./hoc/asyncComponent/asyncComponent";
-import Homepage from "./containers/Homepage/Homepage";
+import Listings from "./containers/Homepage/Listings/Listings";
 
 const asyncNewPost = asyncComponent(() => {
   return import("../src/containers/NewPost/NewPost");
 });
 
 class App extends Component {
-  // componentDidMount() {
-  //   this.props.onTryAutoSignUp();
-  // }
-
   render() {
-    // let routes = (
-    //   <Switch>
-    //     <Redirect to="/" />
-    //   </Switch>
-    // );
-
-    // if (this.props.isAuthenticated) {
-    //   routes = (
-    //     <Switch>
-    //       <Route path="/logout" component={Logout} />
-    //       <Redirect to="/" />
-    //     </Switch>
-    //   );
-    // }
-
     return (
       <div>
         <Layout>
           <Switch>
-            <Route path="/" exact component={Homepage} />
+            <Route path="/" exact component={Listings} />
             <Route path="/new-post" component={asyncNewPost} />
             <Redirect to="/" />
           </Switch>
@@ -47,19 +25,5 @@ class App extends Component {
     );
   }
 }
-
-// const mapStateToProps = (state) => {
-//   return {
-//     isAuthenticated: state.auth.token !== null,
-//   };
-// };
-
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     onTryAutoSignUp: () => dispatch(actions.authCheckState()),
-//   };
-// };
-
-//export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
 
 export default withRouter(App);
