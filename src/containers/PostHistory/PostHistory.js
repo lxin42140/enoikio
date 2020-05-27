ef5e31fb1183adeecd6042f0cf36c5147c87c3b2
[lxin42140] <[lxin42140@gmail.com]>
2020-05-27 13:23:16 +0800

    added post history component

---
+++ src/containers/PostHistory/PostHistory.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import Spinner from "../../components/UI/Spinner/Spinner";
+import firebaseAxios from "../../firebaseAxios";
+import * as actions from "../../store/actions/index";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
+import Listings from "../Listings/Listings";
+
+class PostHistory extends Component {
+  componentDidMount() {
+    this.props.onFetchUserPosts("userID", this.props.userID);
+  }
+
+  render() {
+    let history = this.props.loading ? (
+      <Spinner />
+    ) : (
+      <Listings filtered={true} />
+    );
+    return <div>{history}</div>;
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    listingData: state.listing.listings,
+    loading: state.listing.loading,
+    userID: state.auth.userId,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onFetchUserPosts: (type, userID) =>
+      dispatch(actions.fetchFilteredListing(type, userID)),
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withErrorHandler(PostHistory, firebaseAxios));
