3c90aed3d86fd4ab89e7c84389b94625d24f4d8f
[lxin42140] <[lxin42140@gmail.com]>
2020-05-27 19:30:13 +0800

    fixed bug that caused logout to display filtered content instead of all listings

--- src/containers/Auth/Logout/Logout.js
+++ src/containers/Auth/Logout/Logout.js
@@ -1,24 +1,24 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import * as actions from "../../../store/actions/index";
-import {connect} from "react-redux";
-import {Redirect} from "react-router-dom"
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
 
 class Logout extends Component {
-    componentDidMount() {
-        this.props.onLogout();
-    }
+  componentDidMount() {
+    this.props.onFetchALLListingInit();
+    this.props.onLogout();
+  }
 
-    render() {
-        return (
-            <Redirect to="/" />
-        );
-    }
+  render() {
+    return <Redirect to="/" />;
+  }
 }
 
-const mapDispatchToProps = dispatch => {
-    return {
-        onLogout: () => dispatch(actions.logout())
-    }
-}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onLogout: () => dispatch(actions.logout()),
+    onFetchALLListingInit: () => dispatch(actions.fetchAllListings()),
+  };
+};
 
-export default connect(null, mapDispatchToProps) (Logout);
+export default connect(null, mapDispatchToProps)(Logout);
