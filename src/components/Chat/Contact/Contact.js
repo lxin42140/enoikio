19484517b14c6c329dc67886b064d21ccbf312eb
Li <lxin42140@gmail.com>
2020-06-06 13:50:46 +0800

    added chat component

---
+++ src/components/Chat/Contact/Contact.js
@@ -0,0 +1,35 @@
+import React from "react";
+import classes from "./Contact.css";
+import logo from "../../../assets/Logo/Logo.png";
+
+const contact = (props) => {
+  return (
+    <div className={classes.friend} onClick={props.onClick}>
+      <img src={logo} alt="" />
+      <p>
+        <strong
+          style={{
+            fontWeight: "600",
+            fontSize: "15px",
+            color: "#597a96",
+          }}
+        >
+          {props.userName}
+        </strong>
+        <br />
+        <span
+          style={{
+            fontWeight: "400",
+            fontSize: "13px",
+            color: "#aab8c2",
+          }}
+        >
+          {props.lastMessage}
+        </span>
+      </p>
+      {/* <div className={classes.status}></div> */}
+    </div>
+  );
+};
+
+export default contact;
