4625b963382f2f2c1b2e542d039632d6e7fb9d5a
Li <lxin42140@gmail.com>
2020-06-21 22:38:36 +0800

    test

--- src/containers/Listings/FilteredListings.js
+++ src/containers/Listings/FilteredListings.js
@@ -3,7 +3,6 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
 import Listing from "./Listing/Listing";
-import classes from "./Listings.css";
 
 class FilteredListings extends Component {
   state = {
